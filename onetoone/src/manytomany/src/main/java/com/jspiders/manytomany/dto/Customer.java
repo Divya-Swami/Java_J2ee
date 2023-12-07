1417adb21acb25cc29f2efbe878f1ba025e8e767
Divya Mallikarjum Swami <137381410+Divya-Swami@users.noreply.github.com>
2023-12-07 22:24:25 +0530

    Add files via upload
    Hibernate Mapping - Many  to Many  mapping 
    - When more than one record from one table is releted to exactly  more than one record from table  two then it is called as Many  to Many  mapping.
    -for that  make use of @ManyToMany mapping annotation.

---
+++ onetoone/src/manytomany/src/main/java/com/jspiders/manytomany/dto/Customer.java
@@ -0,0 +1,59 @@
+package com.jspiders.manytomany.dto;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String adress;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	private List<Product> products;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	
+	
+}
